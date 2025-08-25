03ba6e49b6d457564d2ef34c16725d9ccf434dc3
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-08-25 22:46:48 +0200

    parse list and map

--- src/neurohearing/common/tools.py
+++ src/neurohearing/common/tools.py
@@ -5,3 +5,18 @@ def load_config():
     with open('config.yaml') as p:
         config = yaml.safe_load(p)
     return config
+
+def parse_list(arg_str):
+    """Konwertuje string 'a,b,c' na listę ['a','b','c']"""
+    return [x.strip() for x in arg_str.split(',')] if arg_str else []
+
+
+def parse_map(map_str):
+    """
+    Konwertuje string 'Arkusz1=klucz1,Arkusz2=klucz2' na słownik
+    """
+    mapping = {}
+    for pair in map_str.split(','):
+        sheet, key = pair.split('=')
+        mapping[sheet.strip()] = key.strip()
+    return mapping

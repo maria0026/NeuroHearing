d88936d5a50b64718a317113bb56bde208ebe123
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-08-26 17:55:55 +0200

    masking and not masking rows merging

--- src/neurohearing/preprocess/tonal_audiometry.py
+++ src/neurohearing/preprocess/tonal_audiometry.py
@@ -3,9 +3,9 @@ import pandas as pd
 class TonalAudiometry():
     def __init__(self, path, mask_merging_columns=["PESEL", "UWAGI_DO_AUDIOMETRII_t"],
                  date_column="DATA_BADANIA_t",
-                 audiometry_type={"TYP_AUDIOMETRII_t": [["BoneMask", "Bone"], ["AirMask", "Air"]]}):
+                 audiometry_type={"TYP_AUDIOMETRII_t": [["Bone", "BoneMask"], ["Air", "AirMask"]]}):
         
-        self.data = pd.read_csv(path, sep=None, engine='python')
+        self.data = pd.read_csv(path, sep=None, engine='python', dtype={"PESEL": str})
         self.mask_merging_columns = mask_merging_columns
         self.date_column = date_column
         self.audiometry_type = audiometry_type
@@ -24,22 +24,31 @@ class TonalAudiometry():
 
         pairs = [p for sublist in self.audiometry_type[type_col] for p in [sublist]]
 
+        #mini_df for each patient and each ear
         mini_dfs = []
         for _, group in self.data.groupby(group_columns):
             mini_dfs.append(group.reset_index(drop=True))
 
         for mini_df in mini_dfs:
+
             types = set(mini_df[type_col])
             for pair in pairs:
                 if set(pair) <= types:
-                    merged_row = mini_df.drop(columns=[type_col]) \
-                    .agg(lambda x: x.iloc[0] if x.nunique() == 1 else list(x))
-                    print(merged_row.columns)
-                    #print(merged_row[['UWAGI_DO_AUDIOMETRII_t', 'TYP_AUDIOMETRII_t']])
-                #merged_mini_dfs.append(merged_row.to_frame().T)
-                #merged = True
-                #break
-                
+                    row_first = mini_df[mini_df[type_col] == pair[0]].iloc[0]
+                    row_second = mini_df[mini_df[type_col] == pair[1]].iloc[0]
+                    merged_row = row_first.combine_first(row_second)
+
+                    #update values with merged from masking and not masking
+                    idx_first = mini_df[mini_df[type_col] == pair[0]].index[0]
+                    mini_df.loc[idx_first] = merged_row
+                    #delete second row
+                    mini_df = mini_df[mini_df[type_col] != pair[1]]
+
+            #filter vibro
+            mini_df = mini_df[~mini_df[type_col].str.contains("Vibro", na=False)]
+            mini_df = mini_df[~mini_df[type_col].str.contains("szumy", na=False)]
+            #cols = mini_df.filter(like='WYNIK').columns
+            #print(mini_df[list(cols) + ['UWAGI_DO_AUDIOMETRII_t', 'TYP_AUDIOMETRII_t']])
 
 
 

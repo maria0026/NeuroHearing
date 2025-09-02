693ee7c5aa0da7f16e89a0744d2704637df1dfdf
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-09-02 12:28:14 +0200

    reading from config file

--- src/neurohearing/preprocess/read_merge.py
+++ src/neurohearing/preprocess/read_merge.py
@@ -9,17 +9,17 @@ def main(args):
     config = tools.load_config()
     audiometry_datapath = config["datarawdirectory"] + config["dataname"] + '.xlsx'
 
-    audiometry_types = tools.parse_list(args.audiometry_types)
-    audiometry_map = tools.parse_map(args.audiometry_map) if args.audiometry_map else None
-    audiometry_dropcolumns = tools.parse_list(args.audiometry_dropcolumns)
-    match_columns = tools.parse_list(args.match_columns)
+    audiometry_types = tools.parse_list(args.audiometry_types) if args.audiometry_types else config["audiometry_types"]
+    audiometry_map = tools.parse_map(args.audiometry_map) if args.audiometry_map else config["audiometry_map"]
+    audiometry_dropcolumns = tools.parse_list(args.audiometry_dropcolumns) if args.audiometry_dropcolumns else config["audiometry_dropcolumns"]
+    match_columns = tools.parse_list(args.match_columns) if args.match_columns else config["match_columns"]
 
     processor = FileProcessor(
         audiometry_datapath,
         audiometry_types=audiometry_types,
         audiometry_map=audiometry_map,
         audiometry_dropcolumns=audiometry_dropcolumns,
-        match_columns=match_columns,
+        match_columns = match_columns,
         output_path=config["datainterimdirectory"])
     
     processor.read_audiometry()
@@ -30,40 +30,16 @@ def main(args):
     print("Filtering completed")
     print(processor.audiometries)
 
-    processor.merge()
+    processor.merge_audiometries(audiometry_type_column=config["audiometrytypecolumnname"])
     print("Merging completed")
     print(processor.audiometries)
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Parser for audiometry data analyzer")
-    parser.add_argument(
-        "--audiometry_types",
-        nargs="?",
-        default="Audiometria_Tonalna,Audiometria_Slowna,Audiometria_Pole_Swobodne",
-        help="Comma-separated list of audiometry sheet names",
-        type=str
-    )
-    parser.add_argument(
-        "--audiometry_map",
-        nargs="?",
-        default="Audiometria_Tonalna=tonal,Audiometria_Slowna=verbal,Audiometria_Pole_Swobodne=free_field",
-        help="Comma-separated mapping 'SheetName=Key'",
-        type=str
-    )
-    parser.add_argument(
-        "--audiometry_dropcolumns",
-        nargs="?",
-        default="WYNIKI_POZOSTALE,WYKONUJACY_BADANIE,OPIS_BADANIA,id",
-        help="Comma-separated list of columns to drop",
-        type=str
-    )
-    parser.add_argument(
-        "--match_columns",
-        nargs="?",
-        default="PESEL,NUMER_W_JEDNOSTCE,NUMER_HISTORII_CHOROBY",
-        help="Comma-separated list of key columns not to rename",
-        type=str
-    )
+    parser.add_argument("--audiometry_types", help="Comma-separated list of audiometry sheet names")
+    parser.add_argument("--audiometry_map", help="Comma-separated mapping 'SheetName=Key'")
+    parser.add_argument("--audiometry_dropcolumns", help="Comma-separated list of columns to drop")
+    parser.add_argument("--match_columns", help="Comma-separated list of key columns not to rename")
     args = parser.parse_args()
     main(args)

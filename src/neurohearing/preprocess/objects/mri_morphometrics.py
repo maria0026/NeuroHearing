1d95b353d211eaa557a10f38bf8787499ba7dab9
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-09-17 23:45:22 +0200

    filtering dataset

--- src/neurohearing/preprocess/objects/mri_morphometrics.py
+++ src/neurohearing/preprocess/objects/mri_morphometrics.py
@@ -20,6 +20,69 @@ class MRI_morphometics():
         self.data_mri = pd.merge(self.data_mri, mapping, on="identifier")
 
 
+    def filter_age(self, age_label_name):
+        self.data_mri = self.data_mri[(self.data_mri[age_label_name] >= 0) & (self.data_mri[age_label_name] <= 100)].copy()
+
+
+    def filter_zeros(self, filtering_threshold):
+        zero_ratio = (self.data_mri == 0).sum() / len(self.data_mri)
+        nan_ratio = self.data_mri.isna().sum() / len(self.data_mri)
+        cols_to_keep = zero_ratio[(zero_ratio < filtering_threshold) & (nan_ratio < filtering_threshold)].index
+        print(f"Kept {len(cols_to_keep)} columns out of {len(self.data_mri.columns)} after filtering zeros and NaNs")
+        self.data_mri = self.data_mri[cols_to_keep].copy()
+
+        
+
+    def filter_outliers(self, age_label='age', n_std=6, removed_ids_output_path="removed_identifiers.csv", age_bins=None):
+        data_mri_original = self.data_mri.copy()
+        numeric_cols = self.data_mri.select_dtypes(include='number').columns
+
+        '''
+        if age_bins is None:
+            min_age = int(self.data_mri[age_label].min())
+            max_age = int(self.data_mri[age_label].max())
+            age_bins = list(range(min_age, max_age + 10, 10))
+    
+        # przypisz grupy wiekowe
+        self.data_mri['age_group'] = pd.cut(self.data_mri[age_label], bins=age_bins)
+        '''
+
+        mask_total = pd.Series(True, index=self.data_mri.index)
+        outliers_list = []
+
+        for col in numeric_cols:
+            #for age_group, group_df in self.data_mri.groupby('age_group', observed=True):
+            mean = self.data_mri[col].mean()
+            std = self.data_mri[col].std()
+            difference = (self.data_mri[col] - mean) / std
+            mask = (difference >= - n_std) & (difference <= n_std)
+
+            outliers = self.data_mri.loc[~mask, ['identifier', col]].copy()
+            outliers['difference_in_std'] = difference[~mask]
+            outliers['column'] = col
+            outliers_list.append(outliers)
+            mask_total &= mask
+
+        self.data_mri = self.data_mri[mask_total].copy()
+
+        print("Outliers filtered using ±6σ rule.")
+        removed_df = pd.concat(outliers_list, ignore_index=True)
+        removed_df = removed_df.dropna(subset=['difference_in_std'])
+        removed_df = removed_df.loc[removed_df.groupby('identifier')['difference_in_std'].idxmax()]
+
+        print(f"Removed identifiers: {len(removed_df)} before: {data_mri_original.shape} after: {self.data_mri.shape}")
+
+        removed_df = removed_df[['identifier', 'column', 'difference_in_std']]
+        removed_df = removed_df.sort_values(
+            by='difference_in_std', 
+            key=lambda x: x.abs(), 
+            ascending=False
+        )
+
+        removed_df.to_csv(removed_ids_output_path, index=False)
+        print(f"Removed identifiers saved to {removed_ids_output_path}")
+
+
     def merge_with_audiometry(self, data_audiometry):
         self.data_mri_audiometry_tonal = pd.merge(self.data_mri, data_audiometry, on=self.pesel_columnname)
 
@@ -32,7 +95,12 @@ class MRI_morphometics():
         data_mri_audiometry_tonal_sorted = data_mri_audiometry_tonal.sort_values([self.pesel_columnname, f'{self.mri_suffix}_{tonal_suffix}_date_diff'])
         self.data_mri_audiometry_tonal_filtered = data_mri_audiometry_tonal_sorted.groupby(self.pesel_columnname).first().reset_index()
 
-    def save_datasets(self, output_path_merged, output_datapath_mri):    
-        self.data_mri_audiometry_tonal_filtered.to_csv(output_path_merged)
-        train_mri = self.data_mri[~self.data_mri[self.pesel_columnname].isin(self.data_mri_audiometry_tonal_filtered[self.pesel_columnname])]
+
+    def save_datasets(self, output_path_merged, output_datapath_mri): 
+        train_mri = self.data_mri[~self.data_mri[self.pesel_columnname].isin(self.data_mri_audiometry_tonal_filtered[self.pesel_columnname])].copy()
+        #train_mri.drop(columns = self.pesel_columnname, inplace=True)   
         train_mri.to_csv(output_datapath_mri)
+        #self.data_mri_audiometry_tonal_filtered.drop(columns = self.pesel_columnname, inplace=True)   
+        self.data_mri_audiometry_tonal_filtered.to_csv(output_path_merged)
+
+

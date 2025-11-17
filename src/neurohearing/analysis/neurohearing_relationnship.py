21e9fdac298c83b0f6723722e9edc32974ebac1f
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-11-17 16:43:05 +0100

    Create neurohearing_relationnship.py

---
+++ src/neurohearing/analysis/neurohearing_relationnship.py
@@ -0,0 +1,34 @@
+import neurohearing.common.tools as tools
+from neurohearing.preprocess.objects.neuro_audiometry_analyser import NeurohearingAnalyser
+
+def main():
+    config = tools.load_config()
+    tonaldataname=config["tonaldataname"]
+    tonal_suffix = tonaldataname.split("_")[-1]
+    datapath = config["dataprocesseddirectory"] + tonaldataname+'.csv'
+
+
+    neurohearing_analyser = NeurohearingAnalyser(datapath, 
+                                                tonal_suffix, 
+                                                columnnames={'patient_number_columnname': config["patient_number_columnname"],
+                                                            'pesel_columnname': config["pesel_columnname"],
+                                                            'audiometry_earside_columnname': config['audiometry_earside_columnname'],
+                                                            'date_column': config['date_column'],
+                                                            'type_column': config['audiometry_type_columnname'],
+                                                            'description_column': config['description_columnname'],
+                                                            'genetic_patient_id_column': config['patient_identifier_columnname']
+                                                            },
+                                                air_audiometry=config['air_audiometry'],
+                                                bone_audiometry=config['bone_audiometry'],
+                                                vibro_audiometry=config['vibro_audiometry'])
+    
+
+    neurohearing_analyser.patients_dfs()
+    neurohearing_analyser.choose_first_examination()
+    neurohearing_analyser.create_dataframe_for_merging(config["datacalculationsdirectory"])
+    #neurohearing_analyser.save_processed_df(config["datacalculationsdirectory"])
+
+
+if __name__=="__main__":
+
+    main()
